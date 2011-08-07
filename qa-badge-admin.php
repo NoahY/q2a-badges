0ac366e97380ad9219e586e4302cba4ca1b9699d
NoahY <noahy57@gmail.com>
2011-08-07 19:21:35 +0530

    added int check to badge check

--- qa-badge-admin.php
+++ qa-badge-admin.php
@@ -326,7 +326,7 @@
 				
 				if($post['netvotes'] !=0) $users[$user][$pt.'votes'][] = array(
 					'id'=>$pid,
-					'votes'=>$post['netvotes'],
+					'votes'=>(int)$post['netvotes'],
 					'parentid'=>$post['parentid'],
 					'created'=>$post['created']
 				);
@@ -403,7 +403,7 @@
 						if(!isset($data[$pt.'votes'])) continue;
 						foreach($data[$pt.'votes'] as $idv) {
 							
-							if($idv['votes'] >= (int)qa_opt('badge_'.$badge_slug.'_var') && qa_opt('badge_'.$badge_slug.'_enabled') !== '0') {
+							if((int)$idv['votes'] >= (int)qa_opt('badge_'.$badge_slug.'_var') && qa_opt('badge_'.$badge_slug.'_enabled') !== '0') {
 								
 								$result = qa_db_read_one_value(
 									qa_db_query_sub(
