8551ba7cea695a1ee86beb4f44bfe761f0b4cb2a
NoahY <yuttadhammo@yahoo.com>
2011-08-10 00:19:57 +0530

    version

--- qa-plugin.php
+++ qa-plugin.php
@@ -4,7 +4,7 @@
         Plugin Name: Badges
         Plugin URI: 
         Plugin Description: Awards Badges on events
-        Plugin Version: 0.4
+        Plugin Version: 0.7
         Plugin Date: 2011-07-30
         Plugin Author: NoahY
         Plugin Author URI: 
