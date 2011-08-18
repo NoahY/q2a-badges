c6532b02558748b4409d59e100e6abe8088d3158
NoahY <noahy57@gmail.com>
2011-08-18 13:19:22 +0530

    fixing

--- qa-badge-admin.php
+++ qa-badge-admin.php
@@ -121,11 +121,6 @@
 							') ENGINE=MyISAM DEFAULT CHARSET=utf8'
 						);
 						
-						foreach($badges as $slug=>$info) {
-							qa_opt('badge_'.$slug.'_enabled', $this->option_default('badge_'.$slug.'_enabled'));
-							qa_opt('badge_'.$slug.'_var', $this->option_default('badge_'.$slug.'_var'));
-							qa_opt('badge_'.$slug.'_name', $this->option_default('badge_'.$slug.'_name'));
-						}
 					}
 					else {
 						// set badge names, vars and states
