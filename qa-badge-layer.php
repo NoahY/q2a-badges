3f84a4116e442a5e8c01afc2a195d5390b4620b4
NoahY <noahy57@gmail.com>
2011-11-02 22:02:20 +0530

    added option for q_item badges

--- qa-badge-layer.php
+++ qa-badge-layer.php
@@ -193,7 +193,7 @@
 
 		function post_meta_who($post, $class)
 		{
-			if (qa_opt('badge_active') && (bool)qa_opt('badge_admin_user_widget') && isset($post['who'])) {
+			if (qa_opt('badge_active') && (bool)qa_opt('badge_admin_user_widget') && isset($post['who']) && ($class != 'qa-q-item' || qa_opt('badge_admin_user_widget_q_item')) ) {
 				$handle = preg_replace('|.+user/([^"]+)".+|','$1',$post['who']['data']);
 				$post['who']['suffix'] = (@$post['who']['suffix']).'&nbsp;'.$this->user_badge_widget($handle);
 			}
