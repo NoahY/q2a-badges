ffe257c59da4bacf027c12b2e1de421706a8334c
NoahY <noahy57@gmail.com>
2011-11-28 12:57:01 +0530

    version and readme

--- qa-plugin.php
+++ qa-plugin.php
@@ -4,7 +4,7 @@
         Plugin Name: Badges
         Plugin URI: 
         Plugin Description: Awards Badges on events
-        Plugin Version: 1.0b2
+        Plugin Version: 1.1
         Plugin Date: 2011-07-30
         Plugin Author: NoahY
         Plugin Author URI: 
